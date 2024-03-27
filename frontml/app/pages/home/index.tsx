fa54ffb27af1773420e746e4d9256f74f60d4db8
mateusSleite <mateusleite2709@gmail.com>
2024-03-27 15:21:12 -0300

    FrontIniciado

---
+++ frontml/app/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HomePageContainer } from './styled';
+
+const Home: React.FC = () => {
+  return (
+    <HomePageContainer>
+      <h1>Bem-vindo à página inicial!</h1>
+    </HomePageContainer>
+  );
+};
+
+export default Home;
